bf933b1933e122424d5606886acf0cf82303412c
Remy Goldschmidt <taktoa@gmail.com>
2011-05-16 17:14:54 -0400

    BConfig Show instance

--- Debug/BangBang.hs
+++ Debug/BangBang.hs
@@ -7,6 +7,9 @@ data (Ord a, Ord b) => BConfig a b = BConfig    {   threshold :: a,
                                                     highValue :: b
                                                 }
 
+instance (Show a, Show b) => Show (BConfig a b) where
+    show = show
+
 bEvaluate :: (Ord a, Ord b) => BConfig a b -> a -> b
 bEvaluate bcfg input
         | input >= limen        = high
--- Debug/BangBangImp.hs
+++ Debug/BangBangImp.hs
@@ -3,6 +3,8 @@ import Simulation
 import Genetic
 import BangBang
 import Utils
+import System
+import System.IO
 
 evalSim :: DBConfig -> System -> System
 evalSim bcfg (state, scfg) = (updateState, scfg)
@@ -23,7 +25,7 @@ bangFitness iters scfg bcfg = average (map theta (bangSim scfg bcfg iters))
 bangGenetic :: SConfig -> IO DBConfig
 bangGenetic scfg = do
         let (sWeight, mWeight, cWeight) = (0.05, 0.01, 0.7)
-        let (bIters, gIters) = (2000, 1000)
+        let (bIters, gIters) = (500, 1000)
         let chromToBConfig (a:b:c:_) = BConfig a b c
         let fitness = bangFitness bIters scfg . chromToBConfig
         let (popSize, gpc) = (20, 12)
@@ -36,5 +38,16 @@ runBangSim :: IO ()
 runBangSim = do
         let initState = SState 0 0 0.1 0 0 0
         let scfg = protoConfig (0.125, 1, 1, 2, 10, initState)
+        let bIters = 300
         bcfg <- bangGenetic scfg
-        print (map stateTuple (filter (\x -> abs (w x) < 40) (bangSim scfg bcfg 300)))
+        let bang = bangSim scfg bcfg bIters
+        let fitness = bangFitness bIters scfg bcfg
+        let disp = (average (map theta bang))
+        args <- getArgs
+        let fitnessFile:trackFile:_ = args
+        fitnessHandle <- openFile fitnessFile WriteMode
+        hPutStr fitnessHandle (show disp ++ ", " ++ show fitness ++ ", \"" ++ show bcfg ++ "\", ")
+        hClose fitnessHandle
+        trackHandle <- openFile trackFile WriteMode
+        hPrint fitnessHandle bang
+        hClose trackHandle
